9edbf3dd40259e83ee9359ea00e254b9bf1f7718
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2022-10-26 13:11:24 +0200

    All pages linked for the first time

---
+++ app/public/js/deskapp.js
@@ -0,0 +1,21 @@
+let dropdown = document.getElementById('hamburger-btn');
+
+
+let dropdownbox = document.getElementById('nav-mobile');
+console.log(dropdownbox);
+
+dropdown.addEventListener('click', function(){
+    if( dropdownbox.style.display == "none"){
+        
+    dropdownbox.style.display = "block";
+    }
+    else{
+        dropdownbox.style.display = "none";
+    }
+
+});
+
+function closeNav(){
+   document.getElementById('nav-mobile').style.display='none';
+}
+document.querySelector('#main').addEventListener('click',closeNav)
